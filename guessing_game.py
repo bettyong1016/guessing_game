b1546631c84c149ae3de89e429821339b3d833b0
bettyong1016 <75482778+bettyong1016@users.noreply.github.com>
2022-08-12 07:54:18 -0400

    uploading again

---
+++ guessing_game.py
@@ -0,0 +1,86 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+--------------------------------
+
+For this first project we will be using Workspaces. 
+
+NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
+
+"""
+
+import random
+
+
+def start_game():
+    """Psuedo-code Hints
+    
+    When the program starts, we want to:
+    ------------------------------------
+    1. Display an intro/welcome message to the player.
+    2. Store a random number as the answer/solution.
+    3. Continuously prompt the player for a guess.
+      a. If the guess greater than the solution, display to the player "It's lower".
+      b. If the guess is less than the solution, display to the player "It's higher".
+    
+    4. Once the guess is correct, stop looping, inform the user they "Got it"
+         and show how many attempts it took them to get the correct number.
+    5. Let the player know the game is ending, or something that indicates the game is over.
+    
+    ( You can add more features/enhancements if you'd like to. )
+    """
+    # write your code inside this function.
+    print("-----------------------------------")
+    print("Welcome to the Number Guessing Game!")
+    print("-----------------------------------")
+    
+    numberToBeGuessed = random.randint(1,11)
+    
+    count = 0
+    
+    while True:
+        answerStr = input("Pick a number between 1 and 10: ")
+        
+        try:
+            answer = int(answerStr)
+        except ValueError:
+            print("This is not a valid value. Please guess again")
+            
+        if int(answer) <  numberToBeGuessed:
+            print("It is higher!")
+            count+=1
+            
+        elif answer > numberToBeGuessed:
+            print("It is lower!")
+            count+=1
+            
+        else:
+            count+=1
+            print("You got it! It took you {} tries".format(count))
+            break
+    end_game(count)
+            
+def end_game(count):
+    while True:
+        answer = input("Would you like to play again [y]es/[n]o: ")
+        answer = answer.lower()
+        if answer == 'y':
+            print("The last score is {}".format(count))
+            start_game()
+            
+        elif answer == 'n':
+            print("Good bye!")
+            break;
+        else:
+            print("Please enter a valid input! y or n")
+    
+    
+    
+    
+    
+    
+
+
+
+# Kick off the program by calling the start_game function.
+start_game()
